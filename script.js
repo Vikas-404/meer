f431fb2acbb02eacc0dfb209f13c19bc3e3bbb9f
Vikas-404 <vikasassudani909@gmail.com>
2022-05-20 11:02:58 +0530

    Git/Github session

---
+++ script.js
@@ -0,0 +1,9 @@
+const hh = document.getElementById("hh");
+
+hh.addEventListener("mouseover",()=>{
+    hh.innerText = "Palm Tree Club"
+})
+
+hh.addEventListener("mouseleave",()=>{
+    hh.innerText = "Hello World!"
+})
